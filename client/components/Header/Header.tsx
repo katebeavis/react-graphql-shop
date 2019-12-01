1817412bd46f26798409ce882bec40d71d0b631f
Kate Beavis <katebeavis84@gmail.com>
2019-12-01 14:28:23 +0000

    Add nprogress

--- client/components/Header/Header.tsx
+++ client/components/Header/Header.tsx
@@ -1,14 +1,29 @@
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
 import Nav from '../Nav/Nav';
 
 import { Logo, Bar, SubBar } from './style';
 
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done();
+});
+
+Router.events.on('routeChangeError', error => {
+  NProgress.done();
+  console.log('error: ', error);
+});
+
 const Header = () => (
   <>
     <Bar>
       <Logo>
-        <Link href="/">
+        <Link href='/'>
           <a>React GraphQL store</a>
         </Link>
       </Logo>
--- client/components/Meta/Meta.tsx
+++ client/components/Meta/Meta.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head';
 
+import '../../public/nprogress.css';
+
 const Meta = () => (
   <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta charSet="utf-8" />
+    <meta name='viewport' content='width=device-width, initial-scale=1' />
+    <meta charSet='utf-8' />
     <title>React GraphQL store</title>
   </Head>
 );
