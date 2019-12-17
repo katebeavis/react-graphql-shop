830c34845dc1d382b1f51682747cb2351a987ea2
Kate Beavis <katebeavis84@gmail.com>
2019-12-17 09:42:39 +0000

    feat(server): add query and mutation for items (#6)

--- server/src/mutation.ts
+++ server/src/mutation.ts
@@ -1,3 +1,17 @@
-const Mutation = {};
+interface Context {
+  db: any;
+}
+
+const Mutation = {
+  async createItem(parent: any, args: any, context: Context, info: any) {
+    const item = await context.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 export default Mutation;
--- server/src/query.ts
+++ server/src/query.ts
@@ -1,3 +1,12 @@
-const Query = {};
+interface Context {
+  db: any;
+}
+
+const Query = {
+  async items(parent: any, args: any, context: Context, info: any) {
+    const items = await context.db.query.items();
+    return items;
+  }
+};
 
 export default Query;
