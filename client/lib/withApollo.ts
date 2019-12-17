00ffd9c3b4fd7e6c68b83939552843186bb5db58
Kate Beavis <katebeavis84@gmail.com>
2019-12-17 12:06:03 +0000

    Apollo client (#7)
    * refactor(server): remove comment
    
    * feat(client): set up apollo client

---
+++ client/lib/withApollo.ts
@@ -0,0 +1,22 @@
+import withApollo from 'next-with-apollo';
+import ApolloClient from 'apollo-boost';
+import { GRAPHQL_ENDPOINT } from '../config';
+
+const createClient = ({ headers }: any) => {
+  return new ApolloClient({
+    uri:
+      process.env.NODE_ENV === 'development'
+        ? GRAPHQL_ENDPOINT
+        : GRAPHQL_ENDPOINT,
+    request: operation => {
+      operation.setContext({
+        fetchOptions: {
+          credentials: 'include'
+        },
+        headers
+      });
+    }
+  });
+};
+
+export default withApollo(createClient);
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,11 +1,24 @@
-import Layout from "../components/Layout/Layout";
+import Layout from '../components/Layout/Layout';
+import { ApolloProvider } from 'react-apollo';
+import withApollo from '../lib/withApollo';
+import App from 'next/app';
 
-const MyApp = ({ Component, pageProps }: any) => {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-};
+interface IProps extends App {
+  apollo: any;
+}
 
-export default MyApp;
+class MyApp extends App<IProps> {
+  render() {
+    const { Component, pageProps, apollo } = this.props;
+
+    return (
+      <ApolloProvider client={apollo}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default withApollo(MyApp);
