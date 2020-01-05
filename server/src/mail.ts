27084593cb6adedce9abae61005ede79813d7f76
Kate Beavis <katebeavis84@gmail.com>
2020-01-05 18:02:55 +0000

    feat(server): send email with password reset

---
+++ server/src/mail.ts
@@ -0,0 +1,20 @@
+import * as dotenv from 'dotenv';
+const nodemailer = require('nodemailer');
+
+dotenv.config();
+
+export const transport = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
+
+export const emailTemplate = text =>
+  `<div class="email" style="border: 1px solid black; padding: 20px; font-family: sans-serif; line-height: 2; font-size: 20px;">
+  <h2>Hi there!</h2>
+  <p>${text}</p>
+  <p>Thank you!</p>
+  </div>`;
