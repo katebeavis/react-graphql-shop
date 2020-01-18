4e19c5fe68fd159454dd276c03e8174cc530dc2f
Kate Beavis <katebeavis84@gmail.com>
2020-01-18 19:45:57 +0000

    feat(client): add permissions in local state

--- client/components/Permissions/Permissions.tsx
+++ client/components/Permissions/Permissions.tsx
@@ -4,7 +4,8 @@ import { ALL_USERS_QUERY } from '../../queries/queries';
 import Error from '../Error/Error';
 import { Table } from './Permissions.style';
 import { PERMISSIONS } from '../../constants/constants';
-import User from '../User/User';
+import UserPermissions from '../UserPermissions/UserPermissions';
+import { IUser } from '../../shared/types';
 
 const Permissions = () => {
   const { loading, error, data } = useQuery(ALL_USERS_QUERY);
@@ -21,15 +22,19 @@ const Permissions = () => {
           <tr>
             <th>Name</th>
             <th>Email</th>
-            {PERMISSIONS.map((permission: any, index: number) => {
+            {PERMISSIONS.map((permission: string, index: number) => {
               return <th key={index}>{permission}</th>;
             })}
             <th />
           </tr>
         </thead>
         <tbody>
-          {data.users.map((user: any, index: number) => (
-            <User key={index} user={user} permissions={PERMISSIONS} />
+          {data.users.map((user: IUser, index: number) => (
+            <UserPermissions
+              key={index}
+              user={user}
+              permissions={PERMISSIONS}
+            />
           ))}
         </tbody>
       </Table>
