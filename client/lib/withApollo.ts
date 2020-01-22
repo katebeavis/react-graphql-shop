bc14a3aab2aec62246b7d89f4b7a61da9f8287d4
Kate Beavis <katebeavis84@gmail.com>
2020-01-22 18:42:49 +0000

    feat(client): open and close cart

--- client/lib/withApollo.ts
+++ client/lib/withApollo.ts
@@ -1,6 +1,8 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
+
 import { GRAPHQL_ENDPOINT } from '../config';
+import { LOCAL_STATE_QUERY } from '../queries/queries';
 
 const createClient = ({ headers }: any) => {
   return new ApolloClient({
@@ -15,6 +17,21 @@ const createClient = ({ headers }: any) => {
         },
         headers
       });
+    },
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_root, variables, { cache }) {
+            const { cartOpen } = cache.readQuery({ query: LOCAL_STATE_QUERY });
+            const data = { data: { cartOpen: !cartOpen } };
+            cache.writeData(data);
+            return data;
+          }
+        }
+      },
+      defaults: {
+        cartOpen: true
+      }
     }
   });
 };
