56c5d695e8117de3fa8eed40a663ea66b73668f3
Kate Beavis <katebeavis84@gmail.com>
2020-01-26 22:14:12 +0000

    feat(client): refetch query after delete

--- client/components/RemoveItemFromCart/RemoveItemFromCart.tsx
+++ client/components/RemoveItemFromCart/RemoveItemFromCart.tsx
@@ -2,13 +2,16 @@ import { useMutation } from 'react-apollo';
 
 import { RemoveButton } from './RemoveItemFromCart.style';
 import { REMOVE_ITEM_FROM_CART_MUTATION } from '../../mutations/mutations';
+import { CURRENT_USER_QUERY } from '../../queries/queries';
 
 interface RemoveItemFromCartProps {
   id: string;
 }
 
 const RemoveItemFromCart = ({ id }: RemoveItemFromCartProps) => {
-  const [mutate, { loading }] = useMutation(REMOVE_ITEM_FROM_CART_MUTATION);
+  const [mutate, { loading }] = useMutation(REMOVE_ITEM_FROM_CART_MUTATION, {
+    refetchQueries: [{ query: CURRENT_USER_QUERY }]
+  });
   return (
     <RemoveButton
       onClick={() => mutate({ variables: { id } })}
